0218fb09cc87f860331843532993739903abf6d9
eod940 <eod940@gmail.com>
2022-08-22 12:17:02 +0900

    채용공고 등록하기: 채용공고 등록, 채용공고 수정성공시 DetailView로 보여주기

--- employments/urls.py
+++ employments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "employments"
+
 urlpatterns = [
     path('', views.EmploymentsPostListView.as_view()),
-    path('<int:pk>/', views.EmploymentsPostDetailView.as_view()),
-    path('<int:pk>/update/', views.EmploymentsPostUpdateView.as_view()),
+    path('<int:pk>/', views.EmploymentsPostDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', views.EmploymentsPostUpdateView.as_view(), name='update'),
+    path('create/', views.EmploymentsPostCreateView.as_view(), name='create'),
 ]
--- employments/views.py
+++ employments/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse, reverse_lazy
+
 from .models import EmploymentsPost
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 
 
 class EmploymentsPostListView(ListView):
@@ -20,4 +22,31 @@ class EmploymentsPostUpdateView(UpdateView):
         'employments_text',
         'employments_tech_to_use',
     ]
-    success_url = '/employments'
+    success_url = reverse_lazy('employments:detail')
+
+    def get_success_url(self):
+        return reverse('employments:detail', kwargs={'pk':self.object.pk})
+
+
+class EmploymentsPostCreateView(CreateView):
+    model = EmploymentsPost
+    fields = [
+        'employments_position',
+        'employments_compensation',
+        'employments_text',
+        'employments_tech_to_use',
+
+        'company_id',
+    ]
+    success_url = reverse_lazy('employments:detail')
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated():
+            form.instance.employments_author = current_user
+            return super(type(self), self).form_valid(form)
+        else:
+            return redirect('/employments/')
+
+    def get_success_url(self):
+        return reverse('employments:detail', kwargs={'pk':self.object.pk})
